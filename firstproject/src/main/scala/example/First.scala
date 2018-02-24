5bb0f9d242ec3da6269c71e387f540aabd627831
Praveen <Praveen@Praveen>
2018-02-24 13:24:50 +0530

    r6f6t

--- firstproject/src/main/scala/example/First.scala
+++ firstproject/src/main/scala/example/First.scala
@@ -6,7 +6,34 @@ object FirstProject  {
   def main(args: Array[String]): Unit = {
     
     println("Hello scala");
-  }
+    
+    var l = Seq("PRAVEEN PRAVEEN VIKRAM VISHAL KArtheek" ,"VIKRAM VISHAL PRAVEEN")
+    
+    var l1 = l.flatMap(_.split(" "))
+    
+    println("Printing the elements of the spliited ")
+    
+    for (args <- l1) {
+      
+    println(args)
+    
+    }
+    
+    var l2 = l1.map(x => (x,1))
+    println("Printing the word and count")
+    for (args <- l2) {
+      
+    println(args)
+    
+    }
+    
+    }
+      
+    
+    
+  
+    
+  
   
  
 
